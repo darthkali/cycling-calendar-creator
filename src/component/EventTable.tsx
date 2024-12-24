4677a4df20d586c3e860c950389cd364fb69abff
dannysteinbrecher <danny.steinbrecher@outlook.com>
2024-12-24 13:52:41 +0100

    ui changes

--- src/component/EventTable.tsx
+++ src/component/EventTable.tsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import {registerLocale} from 'react-datepicker';
+import enGB from 'date-fns/locale/en-GB';
 import './EventTable.css';
+
+registerLocale('en-GB', enGB);
 
 enum EventType {
     HILL = 'Huegeletappe',
@@ -26,12 +27,35 @@ type Event = {
 
 const EventTable = () => {
     const [events, setEvents] = useState<Event[]>([
-        { stage: '1', date: null, startTime: null, endTime: null, from: '', to: '', kilometers: '', type: EventType.HILL, mountainFinish: false }
+        {
+            stage: '1',
+            date: null,
+            startTime: null,
+            endTime: null,
+            from: '',
+            to: '',
+            kilometers: '',
+            type: EventType.HILL,
+            mountainFinish: false
+        }
     ]);
+
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
 
     useEffect(() => {
         if (events.length === 0) {
-            setEvents([{ stage: '1', date: null, startTime: null, endTime: null, from: '', to: '', kilometers: '', type: EventType.HILL, mountainFinish: false }]);
+            setEvents([{
+                stage: '1',
+                date: null,
+                startTime: null,
+                endTime: null,
+                from: '',
+                to: '',
+                kilometers: '',
+                type: EventType.HILL,
+                mountainFinish: false
+            }]);
         }
     }, [events]);
 
@@ -41,8 +65,18 @@ const EventTable = () => {
         setEvents(newEvents);
     };
 
+    const areRequiredFieldsFilled = (event: Event) => {
+        return event.date && event.startTime && event.endTime;
+    };
+
     const addRow = () => {
         const lastEvent = events[events.length - 1];
+        if (!areRequiredFieldsFilled(lastEvent)) {
+            if (!window.confirm('Du hast Datum, Startzeit und oder Endzeit in der letzten Zeile nicht ausfefüllt. Das solltest du aber tun, da es dir das ausfüllen erleichtert. Willst du dennoch eine neue Zeile einfügen?')) {
+                return;
+            }
+        }
+
         const newDate = lastEvent && lastEvent.date ? new Date(lastEvent.date.getTime() + 86400000) : null; // Add one day (86400000 ms)
         let lastStage = 0;
         for (let i = events.length - 1; i >= 0; i--) {
@@ -85,106 +119,155 @@ const EventTable = () => {
     };
 
     return (
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <div>
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Etappe</th>
-                        <th>Datum</th>
-                        <th>Uhrzeit start</th>
-                        <th>Uhrzeit Ende</th>
-                        <th>Von</th>
-                        <th>Nach</th>
-                        <th>Kilometer</th>
-                        <th>Art</th>
-                        <th>Bergankunft</th>
-                        <th>Aktion</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {events.map((event, index) => (
-                        <tr key={index}>
-                            <td>
-                                <input
-                                    type="text"
-                                    value={event.stage}
-                                    onChange={(e) => handleChange(index, 'stage', e.target.value)}
-                                />
-                            </td>
-                            <td>
-                                <DatePicker
-                                    selected={event.date}
-                                    onChange={(date) => handleChange(index, 'date', date)}
-                                    dateFormat="dd.MM.yyyy"
-                    placeholderText="Datum wählen"
-                                />
-                            </td>
-                            <td>
-                                <TimePicker
-                                    value={event.startTime}
-                                    onChange={(time) => handleChange(index, 'startTime', time)}
-                                    ampm={false}
-                                />
-                            </td>
-                            <td>
-                                <TimePicker
-                                    value={event.endTime}
-                                    onChange={(time) => handleChange(index, 'endTime', time)}
-                                    ampm={false}
-                                />
-                            </td>
-                            <td>
-                                <input
-                                    type="text"
-                                    value={event.from}
-                                    onChange={(e) => handleChange(index, 'from', e.target.value)}
-                                />
-                            </td>
-                            <td>
-                                <input
-                                    type="text"
-                                    value={event.to}
-                                    onChange={(e) => handleChange(index, 'to', e.target.value)}
-                                />
-                            </td>
-                            <td>
-                                <input
-                                    type="text"
-                                    value={event.kilometers}
-                                    onChange={(e) => handleChange(index, 'kilometers', e.target.value)}
-                                />
-                            </td>
-                            <td>
-                                <select
-                                    value={event.type}
-                                    onChange={(e) => handleChange(index, 'type', e.target.value)}
-                                >
-                                    {Object.values(EventType).map((type) => (
-                                        <option key={type} value={type}>{type}</option>
-                                    ))}
-                                </select>
-                            </td>
-                            <td>
-                                <input
-                                    type="checkbox"
-                                    checked={event.mountainFinish}
-                                    onChange={(e) => handleChange(index, 'mountainFinish', e.target.checked)}
-                                />
-                            </td>
-                            <td>
-                                <button onClick={() => deleteRow(index)} className="delete">
-                                    🗑️
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-                <button onClick={addRow}>Add Row</button>
-                <button onClick={exportToICS}>Export to .ics</button>
+        <div>
+            <div className="input-container">
+                <div className="input-group">
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        id="name"
+                        type="text"
+                        placeholder="Name:"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="input-common input-name"
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="description">Beschreibung:</label>
+                    <input
+                        id="description"
+                        type="text"
+                        placeholder="Beschreibung:"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        className="input-common input-description"
+                    />
+                </div>
             </div>
-        </LocalizationProvider>
+            <table>
+                <thead>
+                <tr>
+                    <th>Etappe</th>
+                    <th>Datum</th>
+                    <th>Startzeit</th>
+                    <th>Endzeit</th>
+                    <th>Von</th>
+                    <th>Nach</th>
+                    <th>Kilometer</th>
+                    <th>Art</th>
+                    <th>Bergankunft</th>
+                    <th>Aktion</th>
+                </tr>
+                </thead>
+                <tbody>
+                {events.map((event, index) => (
+                    <tr key={index}>
+                        <td>
+                            <input
+                                type="text"
+                                className="input-stage input-common"
+                                value={event.stage}
+                                onChange={(e) => handleChange(index, 'stage', e.target.value)}
+                            />
+                        </td>
+                        <td>
+                            <DatePicker
+                                selected={event.date}
+                                onChange={(date) => handleChange(index, 'date', date)}
+                                dateFormat="dd.MM.yyyy"
+                                placeholderText="00.00.0000"
+                                locale="en-GB"
+                                className="input-date input-common"
+                            />
+                        </td>
+                        <td>
+                            <DatePicker
+                                selected={event.startTime}
+                                onChange={(time) => handleChange(index, 'startTime', time)}
+                                showTimeSelect
+                                showTimeSelectOnly
+                                placeholderText="00:00"
+                                timeIntervals={5}
+                                timeCaption="Time"
+                                dateFormat="HH:mm"
+                                timeFormat="HH:mm"
+                                locale="en-GB"
+                                className="input-start-time input-common"
+                            />
+                        </td>
+                        <td>
+                            <DatePicker
+                                selected={event.endTime}
+                                onChange={(time) => handleChange(index, 'endTime', time)}
+                                showTimeSelect
+                                showTimeSelectOnly
+                                placeholderText="00:00"
+                                timeIntervals={5}
+                                timeCaption="Time"
+                                dateFormat="HH:mm"
+                                timeFormat="HH:mm"
+                                locale="en-GB"
+                                className="input-end-time input-common"
+                            />
+                        </td>
+                        <td>
+                            <input
+                                type="text"
+                                className="input-from input-common"
+                                placeholder={"Berlin"}
+                                value={event.from}
+                                onChange={(e) => handleChange(index, 'from', e.target.value)}
+                            />
+                        </td>
+                        <td>
+                            <input
+                                type="text"
+                                className="input-to input-common"
+                                placeholder={"Paris"}
+                                value={event.to}
+                                onChange={(e) => handleChange(index, 'to', e.target.value)}
+                            />
+                        </td>
+                        <td>
+                            <input
+                                type="text"
+                                className="input-kilometers input-common"
+                                placeholder={"0"}
+                                value={event.kilometers}
+                                onChange={(e) => handleChange(index, 'kilometers', e.target.value)}
+                            />
+                        </td>
+                        <td>
+                            <select
+                                className="input-type input-common"
+                                value={event.type}
+                                onChange={(e) => handleChange(index, 'type', e.target.value)}
+                            >
+                                {Object.values(EventType).map((type) => (
+                                    <option key={type} value={type}>{type}</option>
+                                ))}
+                            </select>
+                        </td>
+                        <td>
+                            <input
+                                type="checkbox"
+                                className="input-mountain-finish input-common"
+                                checked={event.mountainFinish}
+                                onChange={(e) => handleChange(index, 'mountainFinish', e.target.checked)}
+                            />
+                        </td>
+                        <td>
+                            <button onClick={() => deleteRow(index)} className="delete">
+                                <i className="fas fa-trash-alt"></i>
+                            </button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            <button onClick={addRow}>Add Row</button>
+            <button onClick={exportToICS}>Export to .ics</button>
+        </div>
     );
 };
 
