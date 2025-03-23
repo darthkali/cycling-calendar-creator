cb6cf758dc1ed0b8f42e37874bf45f0b816682ad
dannysteinbrecher <danny.steinbrecher@outlook.com>
2025-03-23 12:38:35 +0100

    added logo

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # CCC - Cycling Calendar Creator
-![logo.png](assets/images/logo.png) (logo was created with mistral.ai)
+
+<div style="display: flex; justify-content: center;">
+<img src="assets/images/logo.png" alt="Beschreibung des Bildes" width="300">
+</div>
+ (logo was created with mistral.ai)
+
 ## Features
 - create an event like the Tour de France
 - add stages
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import EventTable from './component/EventTable';
 function App() {
     return (
         <div className="App">
-            <h1>Radsport Event Planer</h1>
+            <div className="caption"><img className={'logo'} src="/assets/images/logo.png" width={100} height={100} alt=""/> <h1> Radsport Event Planer</h1></div>
+
             <EventTable />
         </div>
     );
